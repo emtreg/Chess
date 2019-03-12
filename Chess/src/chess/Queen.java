626822840e536892c4bcb46d0f25e82110538342
Emily Tregelles <etregelles@gmail.com>
2019-03-11 21:49:39 -0400

    Queen class

--- Chess/src/chess/Queen.java
+++ Chess/src/chess/Queen.java
@@ -4,32 +4,193 @@ public class Queen extends Piece{
 	
 	String pieceType = "queen";
 	
-	public Queen(Tile currentTile, String color)
-	{
+	static final int maxMoves = 27;
+	
+	public Queen(Tile currentTile, String color) {
+		
 		super(currentTile, color);
-		if (color.equals("white"))
-		{
+		
+		if (color.equals("white")) {
 			tag = "wQ";
-		}
-		else
-		{
+			
+		} else {
 			tag = "bQ";
 		}
 	}
 	
-	public void move(Tile end)
-	{
+	public void move(Tile end) {
 		
+		if (isValidPath(currentTile, end) == true) {
+			Tile moves[] = possibleMove();
+			
+			for (int i = 0; i < moves.length; i++) {
+				
+				if (end.equals(moves[i])) {			
+					currentTile.isOccupied = false;
+					currentTile = end;
+				}
+			}
+		}		
 	}
 	
-	public boolean isValidPath(Tile start, Tile end)
-	{
+	/* if only file changes or only rank changes it is a valid (lateral) move
+	 * if file and rank both increase/decrease by the same amount, file increases and rank decreases by the same amount,
+	 * or file decreases and rank increases by the same amount then it is a valid (diagonal) move*/
+	
+	public boolean isValidPath(Tile start, Tile end) {
+		
+		int startLet = start.letter_rank;
+		int startNum = start.number_rank;
+		int endLet = end.letter_rank;
+		int endNum = end.number_rank;
+		
+		if(startLet == endLet && startNum != endNum
+				|| startLet != endLet && startNum == endNum
+				|| startLet - endLet == startNum - endNum 
+				|| endLet - startLet == endNum - startNum
+				|| startLet - endLet == endNum - startNum 
+				|| endLet - startLet == startNum - endNum) {
+		
 		return true;
+		
+		}
+		
+		return false;
 	}
 
-	public Tile[] possibleMove()
-	{
-		Tile[] reachableTiles = new Tile[14];
+	public Tile[] possibleMove() {
+		
+		Tile[] reachableTiles = new Tile[maxMoves];
+		int cnt = 0;
+		
+		//First check forward tiles
+		for (int i = currentTile.number_rank; i < 8; i++) {
+			if (Board.chess_board[i][currentTile.letter_rank].isOccupied == false) {
+				reachableTiles[cnt] = Board.chess_board[i][currentTile.letter_rank];
+				cnt++;
+			} else {
+				if (Board.chess_board[i][currentTile.letter_rank].occupying_piece.color.equals(color)) {
+					break;
+				} else {
+					reachableTiles[cnt] = Board.chess_board[i][currentTile.letter_rank];
+					cnt++;
+					break;
+				}
+			}
+		}
+				
+		//Then check backward tiles
+		for (int i = currentTile.number_rank; i >= 0; i--) {
+			if (Board.chess_board[i][currentTile.letter_rank].isOccupied == false) {
+				reachableTiles[cnt] = Board.chess_board[i][currentTile.letter_rank];
+				cnt++;
+			} else {
+				if (Board.chess_board[i][currentTile.letter_rank].occupying_piece.color.equals(color)) {
+					break;
+				} else {
+					reachableTiles[cnt] = Board.chess_board[i][currentTile.letter_rank];
+					cnt++;
+					break;
+				}
+			}
+		}
+				
+		//Then check right tiles
+		for (int i = currentTile.letter_rank; i < 8; i++) {
+			if (Board.chess_board[currentTile.number_rank][i].isOccupied == false) {
+				reachableTiles[cnt] = Board.chess_board[i][currentTile.letter_rank];
+				cnt++;
+			} else {
+				if (Board.chess_board[currentTile.number_rank][i].occupying_piece.color.equals(color)) {
+					break;
+				} else {
+					reachableTiles[cnt] = Board.chess_board[i][currentTile.letter_rank];
+					cnt++;
+					break;
+				}
+			}
+		}
+				
+		//Finally check left tiles
+		for (int i = currentTile.letter_rank; i >= 0; i--) {
+			if (Board.chess_board[currentTile.number_rank][i].isOccupied == false) {
+				reachableTiles[cnt] = Board.chess_board[i][currentTile.letter_rank];
+				cnt++;
+			} else {
+				if (Board.chess_board[currentTile.number_rank][i].occupying_piece.color.equals(color)) {
+					break;
+				} else {
+					reachableTiles[cnt] = Board.chess_board[i][currentTile.letter_rank];
+					cnt++;
+					break;
+				}
+			}
+		}
+								
+		//check upper right diagonals
+		for(int x = currentTile.letter_rank, y = currentTile.number_rank; x < 8 && y < 8; x++, y++) {		
+			if(Board.chess_board[x][y].isOccupied) {			
+				if(Board.chess_board[x][y].getOccupyingPiece().color.equals(color)) {				
+					break;			
+				} else {					
+					possibleMoves[cnt] = Board.chess_board[x][y];
+					cnt++;
+					break;
+				}			
+			} else {				
+				possibleMoves[cnt] = Board.chess_board[x][y];
+				cnt++;
+			}			
+		}
+				
+		//check lower left diagonals					
+		for(int x = currentTile.letter_rank, y = currentTile.number_rank; x >= 0 && y >= 0; x--, y--) {				
+			if(Board.chess_board[x][y].isOccupied) {			
+				if(Board.chess_board[x][y].getOccupyingPiece().color.equals(color)) {				
+					break;			
+				} else {					
+					possibleMoves[cnt] = Board.chess_board[x][y];
+					cnt++;
+					break;
+				}			
+			} else {				
+				possibleMoves[cnt] = Board.chess_board[x][y];
+				cnt++;
+			}			
+		}
+				
+		//check lower right diagonals				
+		for(int x = currentTile.letter_rank, y = currentTile.number_rank; x < 8 && y >= 0; x++, y--) {					
+			if(Board.chess_board[x][y].isOccupied) {			
+				if(Board.chess_board[x][y].getOccupyingPiece().color.equals(color)) {				
+					break;			
+				} else {					
+					possibleMoves[cnt] = Board.chess_board[x][y];
+					cnt++;
+					break;
+				}			
+			} else {				
+				possibleMoves[cnt] = Board.chess_board[x][y];
+				cnt++;
+			}			
+		}
+				
+		//check upper left diagonals				
+		for(int x = currentTile.letter_rank, y = currentTile.number_rank; x >= 0 && y < 8; x--, y++) {					
+			if(Board.chess_board[x][y].isOccupied) {			
+				if(Board.chess_board[x][y].getOccupyingPiece().color.equals(color)) {				
+					break;			
+				} else {					
+					possibleMoves[cnt] = Board.chess_board[x][y];
+					cnt++;
+					break;
+				}			
+			} else {				
+				possibleMoves[cnt] = Board.chess_board[x][y];
+				cnt++;
+			}			
+		}
+				
 		return reachableTiles;
 	}
 
